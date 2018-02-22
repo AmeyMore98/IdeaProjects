41b4e8a1aa4ac106af1ed01fecd9295fa9c3eed1
Amey More <amey8976@gmail.com>
2018-02-22 19:52:52 +0530

    Tut4_a

---
+++ Tut4_a/src/Models/calModel.java
@@ -0,0 +1,25 @@
+package Models;
+
+/**
+ * Created by Amay on 2/22/2018.
+ */
+public class calModel {
+    private String expr;
+    private int result;
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int r) {
+        this.result = r;
+    }
+
+    public String getExpr() {
+        return expr;
+    }
+
+    public void setExpr(String e) {
+        this.expr = e;
+    }
+}
---
+++ Tut4_a/src/Views/cmd.java
@@ -0,0 +1,27 @@
+package Views;
+
+/**
+ * Created by Amay on 2/22/2018.
+ */
+import java.io.*;
+import java.util.*;
+import Controllers.*;
+
+public class cmd {
+    private String exp = new String();
+    private int result;
+    public String getExp(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Expression:");
+        exp = sc.nextLine();
+        if(exp.length() > 20){
+            System.out.println("Maximum length of 20");
+            getExp();
+        }
+        return exp;
+    }
+
+    public void display(int res) {
+        System.out.println("Result: " + res);
+    }
+}
